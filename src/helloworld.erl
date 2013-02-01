c70a91f19fad68a08b3241e1c78c6bcb814372fb
Syver Enstad <syver.enstad@cisco.com>
2013-02-01 15:16:13 +0100

    Added a simple starting point for a webtool based leaderboard.
    TODO: formatting and proper naming

---
+++ src/helloworld.erl
@@ -0,0 +1,42 @@
+-module(helloworld).
+-export([config_data/0]).
+-export([helloworld/2]).
+
+config_data()->
+    {testtool,
+     [{web_data,{"TestTool","/testtool/helloworld/helloworld"}},
+      {alias,{erl_alias,"/testtool",[helloworld]}}]}.
+
+helloworld(_Env,_Input)->
+    io:format("Env: ~p~nInput: ~p~n", [_Env, _Input]),
+    [header(),html_header(),leaderboard_body(),html_end()].
+
+header() ->
+    header("text/html").
+
+header(MimeType) ->
+    "Content-type: " ++ MimeType ++ "\r\n\r\n".
+
+html_header() ->    
+    "<HTML>
+               <HEAD>
+        <TITLE>Hello world Example </TITLE>
+        </HEAD>\n".
+
+helloworld_body()->
+    "<BODY>Hello World</BODY>".
+
+
+leaderboard_body() ->
+    io:format("leaderboard:~p~n", [leaderboard:list()]),
+    %"<BODY>Hello World</BODY>".
+    {ok, List} = leaderboard:list(),
+    create_html_list(List).
+
+
+html_end()->
+    "</HTML>".
+
+
+create_html_list(List) ->
+    lists:flatten(io_lib:format("~p~n", [List])).
